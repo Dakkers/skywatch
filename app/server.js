374160a3250fd86655e417b3830ccb3519a104a6
Dakota St. Laurent <dstlaurent@gobonfire.com>
2016-04-09 18:09:34 -0400

    SKY-7 removed passportJS as a dependency, using JWT for auth

--- app/server.js
+++ app/server.js
@@ -7,19 +7,17 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 var errorHandler = require('errorhandler');
-var lusca = require('lusca');
 var methodOverride = require('method-override');
+var ejwt = require('express-jwt');
 
-var PostgresStore = require('connect-pg-simple')(session);
 var flash = require('express-flash');
 var path = require('path');
-var passport = require('passport');
 var expressValidator = require('express-validator');
 var connectAssets = require('connect-assets');
 var nev = require('email-verification');
 var bluebird = require('bluebird');
 var pgp = require('pg-promise')({
-  promiseLib: bluebird
+    promiseLib: bluebird
 });
 
 var User = require('./models/User');
@@ -36,51 +34,43 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(compress());
 app.use(connectAssets({
-  paths: [path.join(__dirname, '../public/css'), path.join(__dirname, '../public/js')]
+    paths: [path.join(__dirname, '../public/css'), path.join(__dirname, '../public/js')]
 }));
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
 app.use(methodOverride());
 app.use(cookieParser());
-app.use(session({
-  resave: true,
-  saveUninitialized: true,
-  secret: secrets.sessionSecret,
-  store: new PostgresStore({
-    pg: pgp.pg,
-    conString: secrets.db,
-    autoReconnect: true
-  })
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
-app.use(lusca({
-  csrf: true,
-  xframe: 'SAMEORIGIN',
-  xssProtection: true
-}));
-app.use(function(req, res, next) {
-  res.locals.user = req.user;
-  next();
+app.use(function (req, res, next) {
+
+    if (/api/i.test(req.path)) {
+        req.session.returnTo = req.path;
+    }
+    next();
 });
-app.use(function(req, res, next) {
-  if (/api/i.test(req.path)) {
-    req.session.returnTo = req.path;
-  }
-  next();
-});
+
 app.use(express.static(path.join(__dirname, '../public'), { maxAge: 31557600000 }));
 
+app.use(ejwt({
+    secret: process.env.SKYWATCH_SESSION_SECRET,
+    credentialsRequired: false,
+    getToken: function fromHeaderOrQuerystring (req) {
+
+        if (req.cookies.authorization && req.cookies.authorization.split(' ')[0] === 'Bearer') {
+            return req.cookies.authorization.split(' ')[1];
+        }
+        return null;
+  }
+}));
 
 // error handler
 app.use(errorHandler());
-require('./routes/routes')(app, passport, db);
+require('./routes/routes')(app, db);
 
-app.listen(app.get('port'), function() {
-  console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
+app.listen(app.get('port'), function () {
+
+    console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
 });
 
 module.exports = app;
